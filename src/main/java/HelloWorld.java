f2ee450fd9d362205f4d64bbf5c85263b55aa03d
Yamkela <owamjames1@gmail.com>
2019-12-06 13:02:45 +0200

    adding new changes

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -7,9 +7,25 @@ import java.util.Map;
 import static spark.Spark.*;
 
     public class HelloWorld {
+
+        static int getHerokuAssignedPort() {
+            ProcessBuilder processBuilder = new ProcessBuilder();
+            if (processBuilder.environment().get("PORT") != null) {
+                return Integer.parseInt(processBuilder.environment().get("PORT"));
+            }
+            return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+        }
         public static void main(String[] args) {
 
             staticFileLocation("/public");
+            port(getHerokuAssignedPort());
+
+
+            get("/", (req, res) -> {
+                return new ModelAndView(new HashMap<>(), "hello.hbs");
+            }, new HandlebarsTemplateEngine());
+
+
             get("/hello", (req, res) -> {
 
                 Map<String, String> dataMap = new HashMap<>();
